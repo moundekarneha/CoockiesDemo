a7e51051351661f7996620aa8100fd999bfb15a5
HP <HP@DESKTOP-G4J7E8F>
2022-07-21 17:25:53 +0530

    Add project files.

---
+++ CoockiesDemo/WebForm1.aspx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CoockiesDemo
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            HttpCookie c = new HttpCookie("mycookie");
+            //c.Value = TextBox1.Text;
+            c["ids"] = TextBox1.Text;
+            c["name"] = TextBox2.Text;
+            c.Expires = DateTime.Now.AddSeconds(30);
+            Response.Cookies.Add(c);
+            Response.Redirect("WebForm2.aspx");
+
+
+        }
+    }
+}
